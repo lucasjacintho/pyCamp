648baa2c6cef850e86a067442ca4ee08b930eef8
João Lucas <joao.jacintho@bandtec.com.br>
2020-01-04 00:42:54 -0200

    Padrões de código

---
+++ Exemplos/pep8.py
@@ -0,0 +1,56 @@
+"""
+PEP8 - Python Ennhancement Proposal
+
+São Propostas de melhorias para a linguagem Python
+
+The Zen of Python
+
+import this
+
+A Ideia da PEP8 é que possamos escrever códigos Python de forma Pythônica
+
+[1] - Utilize Camel Case para nomes de classes;
+
+    class Calculadora:
+        pass
+
+    class CalculadoraCientifica:
+        pass
+
+
+
+[2] - Utilize nomes minúsculos, separados por underline para funções ou variaveis;
+
+    def soma():
+        pass
+
+    def soma_dois():
+        pass
+
+    numero = 4
+
+    numero_impar = 5
+
+[3] - Sempre utilizar 4 espaços (TAB) para identaçãos
+
+[4] - Linhas em branco
+- Separar funções e definições de classe com duas linhas em branco;
+- Métodos dentro de uma classe devem ser separadas com uma única linha em branco;
+
+[5] - Imports
+- Os imports devem ser feitos separadamente
+- Caso sejá necessario fazer mais de um import de um mesmo pacote usar
+    from types import(
+        MEUS IMPORTS
+    )
+- Imports sempre no inicio da aplicação
+
+[6] - Espaços em expressões de funções
+- Não usar espaços nas expressões
+    funcao(algo[1], {outro: 2}) // Maneira Correta
+
+[7] - Terminar uma instrução com uma nova linha
+"""
+
+
+
